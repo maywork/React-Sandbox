30243478903b3c3f44055378e137e96ad7af5e1f
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-01 23:16:04 +0600

    added new paradigm

--- 01live-update/src/App.jsx
+++ 01live-update/src/App.jsx
@@ -8,7 +8,24 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-gray-400 font-bold'>Live Text Update App</h1>
+        <h1 className='text-gray-400 font-bold underline'>Live Text Update App</h1>
+        <br />
+        {/* <form onSubmit={(e) => {
+          e.preventDefault()
+          setInputValue(e.target.textField.value)
+          e.target.reset()
+        }}>
+          <input name='textField' className='py-2 px-10 rounded-full border-b border-purple-400' type="text" placeholder='enter text...'
+          />
+          <br />
+          <br />
+          <button
+            type='submit'
+            className='rounded-full bg-purple-400 bg-opacity-25 text-purple-400 border-b border-b-purple-400 border-r-purple-400'
+          >
+            Update
+          </button>
+        </form> */}
         <br />
         <form onSubmit={(e) => {
           e.preventDefault()
