00efdfdb5a43c9bd7664963e2a9767b525f0441e
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-06 23:30:30 +0600

    added base layout of the project

---
+++ 02docs-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tight text-zinc-400'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
