cb2f3e4938a7852172c6f9876cf01d3e6e82778b
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-13 23:51:17 +0600

    added navbar and completed bg

--- 02docs-app/src/App.jsx
+++ 02docs-app/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tight font-semibold text-zinc-200'>Docs</h1>
+      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 font-semibold text-xl'>Documents.</div>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tighter font-semibold text-zinc-900'>Docs.</h1>
     </div>
   )
 }
