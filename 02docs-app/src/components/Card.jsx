0f5bc90ff5cfed0b639f0fcae2f97cff60fdb310
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-16 13:28:37 +0600

    added base card styling

--- 02docs-app/src/components/Card.jsx
+++ 02docs-app/src/components/Card.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
 
 function Card() {
   return (
-    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+    <div className='relative w-60 h-72 rounded-[20px] bg-zinc-900/90 text-purple-200 p-5 overflow-hidden'>
+      <FaRegFileAlt />
+      <p className='text-sm leading-tight mt-5 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <footer className='absolute bottom-0 w-full py-3 px-8 left-0'>
+        <div className='flex items-center justify-between mb-5'>
+          <h5>0.4mb</h5>
+          <span className='w-5 h-5 bg-[#8a2be2] rounded-full flex items-center justify-center'>
+            <LuDownload size='.8em' color='#18181b' />
+          </span>
+        </div>
+      </footer>
 
     </div>
   )
