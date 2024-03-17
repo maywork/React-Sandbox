89446d6d21448c646293caceb73b86fbb43b13cf
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-17 11:38:21 +0600

    added final card detailing

--- 02docs-app/src/components/Card.jsx
+++ 02docs-app/src/components/Card.jsx
@@ -7,12 +7,15 @@ function Card() {
     <div className='relative w-60 h-72 rounded-[20px] bg-zinc-900/90 text-purple-200 p-5 overflow-hidden'>
       <FaRegFileAlt />
       <p className='text-sm leading-tight mt-5 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <footer className='absolute bottom-0 w-full py-3 px-8 left-0'>
-        <div className='flex items-center justify-between mb-5'>
+      <footer className='absolute bottom-0 w-full left-0'>
+        <div className='flex items-center justify-between py-2 px-8 mb-5'>
           <h5>0.4mb</h5>
-          <span className='w-5 h-5 bg-[#8a2be2] rounded-full flex items-center justify-center'>
+          <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
             <LuDownload size='.8em' color='#18181b' />
           </span>
+        </div>
+        <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+          <h3 className='text-md font-semibold'>Download Now</h3>
         </div>
       </footer>
 
