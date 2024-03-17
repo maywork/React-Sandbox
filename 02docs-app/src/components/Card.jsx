41dc27fdc4f92b721d3f0b4d94ce6e60bc32c170
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-17 12:04:32 +0600

    added atg functionality

--- 02docs-app/src/components/Card.jsx
+++ 02docs-app/src/components/Card.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-function Card() {
+function Card({ data }) {
   return (
     <div className='relative w-60 h-72 rounded-[20px] bg-zinc-900/90 text-purple-200 p-5 overflow-hidden'>
       <FaRegFileAlt />
-      <p className='text-sm leading-tight mt-5 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <p className='text-sm leading-tight mt-5 font-medium'>{data.desc}</p>
       <footer className='absolute bottom-0 w-full left-0'>
         <div className='flex items-center justify-between py-2 px-8 mb-5'>
-          <h5>0.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-            <LuDownload size='.8em' color='#18181b' />
+            {data.close ? <IoClose /> : <LuDownload size='.8em' color='white' />}
           </span>
         </div>
-        <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-          <h3 className='text-md font-semibold'>Download Now</h3>
-        </div>
+        {
+          data.tag.isOpen && (
+            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+              <h3 className='text-md font-semibold'>Download Now</h3>
+            </div>
+          )
+        }
+
       </footer>
 
     </div>
--- 02docs-app/src/components/Foreground.jsx
+++ 02docs-app/src/components/Foreground.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+  // blueprint
+  // const data = [
+  //   icon, desc, filesize, closeOrDownload, tagDetails
+  // ]
+
+  const data = [
+    {
+      desc: 'This is the first card. Lorem ipsum dolor sit amet consectetur adipisicing.',
+      filesize: '0.9mb',
+      close: false,
+      tag: { isOpen: false, tagTitle: 'Download Now', tagColor: 'green' }
+    }
+  ]
+
+
   return (
     <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-      <Card />
+      {data.map((item, index) => (
+        <Card data={item} />
+      ))}
     </div>
   )
 }
