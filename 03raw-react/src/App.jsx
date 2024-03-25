8572b386466317f1d3591c482c25962bd728b8bf
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-03-25 16:23:01 +0600

    added tailwind to the project

--- 03raw-react/src/App.jsx
+++ 03raw-react/src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='main'>
-      <h1>App</h1>
-    </div>
+    <>
+      <div className='w-full h-screen bg-zinc-900'>
+        <div className='w-44 h-32 rounded-xl bg-red-600 p-3 text-white'>
+          <h3>hello kaise ho</h3>
+        </div>
+      </div>
+    </>
   )
 }
 
