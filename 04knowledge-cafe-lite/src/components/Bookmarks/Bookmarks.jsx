27c2783f370d10117e4e03857d1446a43de19700
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-04-22 13:57:27 +0600

    added server address

--- 04knowledge-cafe-lite/src/components/Bookmarks/Bookmarks.jsx
+++ 04knowledge-cafe-lite/src/components/Bookmarks/Bookmarks.jsx
@@ -5,7 +5,7 @@ function Bookmarks({ bookmarks, readingTime }) {
   return (
     <div className='md:w-1/3 bg-purple-100 p-4 m-4 rounded-md'>
       <div>
-        <h3 className='text-md text-purple-700'>Reading Rime: <span className='font-semibold'>{readingTime} minutes</span></h3>
+        <h3 className='text-md text-purple-700'>Reading Time: <span className='font-semibold'>{readingTime} minutes</span></h3>
       </div>
       <h3 className="text-3xl py-2 border-b border-purple-600 text-purple-950">Bookmarked Blogs: {bookmarks.length}</h3>
       {
