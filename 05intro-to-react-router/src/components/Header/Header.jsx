c40a53e05cd21ec378f20acb7924e8b96a0ddc14
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-04-25 02:03:12 +0600

    added navlink instead of link for active state

--- 05intro-to-react-router/src/components/Header/Header.jsx
+++ 05intro-to-react-router/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Header.css'
 
 
@@ -8,11 +8,11 @@ function Header() {
       {/* <h3>My WebSite</h3> */}
       <h2>Navbar</h2>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact Us</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
       </nav>
     </div>
   )
