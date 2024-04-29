9428f9eb4b4ef2ec6c81edb4dce365c403f005c0
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-04-29 17:28:41 +0600

    added custom hook using object

--- 06form-master/src/components/HookForm/HookForm.jsx
+++ 06form-master/src/components/HookForm/HookForm.jsx
@@ -3,18 +3,19 @@ import useInputState from '../../hooks/useInputState';
 
 function HookForm() {
 
-  const [name, handleNameChange] = useInputState("")
+  // const [name, handleNameChange] = useInputState("")
+  const emailState = useInputState('email@hooked.go')
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('Form Data, ', name);
+    console.log('Form Data,', emailState.value);
   }
   return (
     <div>
       <form onSubmit={handleSubmit} className="p-4">
-        <input value={name} onChange={handleNameChange} className="rounded-md p-1 mb-2 text-center" placeholder="Please, enter your name" type="text" name="name" />
+        {/* <input value={name} onChange={handleNameChange} className="rounded-md p-1 mb-2 text-center" placeholder="Please, enter your name" type="text" name="name" /> */}
         <br />
-        <input className="rounded-md p-1 mb-2 text-center" placeholder="Please, enter your email" type="email" name="email" />
+        <input {...emailState} className="rounded-md p-1 mb-2 text-center" placeholder="Please, enter your email" type="email" name="email" />
         <br />
         <input className='rounded-md p-1 mb-2 text-center' placeholder="Enter password" type="password" name="password" required />
         <br />
