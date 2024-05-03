56e4bf478b74a79c33a87f49c849d48d039967bd
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-05-03 14:06:58 +0600

    added final title

--- 06form-master/src/App.jsx
+++ 06form-master/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <h1 className='font-bold'>Simple Form</h1>
+      <h1 className='font-bold'>Form Master</h1>
       {/* <Form /> */}
       {/* <StatefulForm /> */}
       {/* <RefForm /> */}
